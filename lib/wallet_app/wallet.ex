98e476dfaebc7d10311425e89277b79458d9cda8
Jethro De Guzman <jethdeguzman@gmail.com>
2018-11-04 23:58:10 +0800

    Add initial wallet module
    This contains create_wallet function
    
    Signed-off-by: Jethro De Guzman <jethdeguzman@gmail.com>

---
+++ lib/wallet_app/wallet.ex
@@ -0,0 +1,17 @@
+defmodule WalletApp.Wallet do
+  import WalletApp.Account, only: [get_current_account: 1]
+  alias WalletApp.Repo
+  alias WalletApp.Schema.Account
+  alias WalletApp.Schema.Wallet
+
+  def create_wallet(session_token, currency \\ "PHP") do
+    %Account{id: account_id} = get_current_account(session_token)
+    %Wallet{}
+      |> Wallet.changeset(%{account_id: account_id, currency: currency})
+      |> Repo.insert
+      |> create_wallet_response
+  end
+
+  defp create_wallet_response({:error, _}), do: raise "Validation error"
+  defp create_wallet_response({:ok, %Wallet{uuid: uuid}}), do: uuid
+end
