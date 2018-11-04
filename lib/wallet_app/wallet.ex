6411c28dd91e1684389b8f8c71232b6d60aef33c
Jethro De Guzman <jethdeguzman@gmail.com>
2018-11-05 00:44:07 +0800

    Implement get_wallets
    Signed-off-by: Jethro De Guzman <jethdeguzman@gmail.com>

--- lib/wallet_app/wallet.ex
+++ lib/wallet_app/wallet.ex
@@ -1,5 +1,5 @@
 defmodule WalletApp.Wallet do
-  import WalletApp.Util, only: [get_current_account: 1]
+  import WalletApp.Util, only: [get_current_account: 1, get_account_wallets: 1]
   
   alias WalletApp.Repo
   alias WalletApp.Schema.Account
@@ -17,7 +17,20 @@ defmodule WalletApp.Wallet do
   defp create_wallet_response({:ok, %Wallet{uuid: uuid}}), do: uuid
 
   def get_wallets(session_token) do
-    %Account{id: account_id} = get_current_account(session_token)
-
+    with(
+      %Account{id: account_id} <- get_current_account(session_token),
+      %{rows: wallets} <- get_account_wallets(account_id)
+    ) do
+      Enum.map wallets, fn [uuid, currency, balance, inserted_at] ->
+        %{
+          uuid: uuid,
+          currency: currency,
+          balance: balance,
+          inserted_at: inserted_at
+        }
+      end
+    else
+      _ -> raise "Error fetching wallets for #{:account_id}"
+    end
   end
 end
