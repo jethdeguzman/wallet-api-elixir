a93ea846db63182e3caf91487302c060b2b849fe
Jethro De Guzman <jethdeguzman@gmail.com>
2018-11-05 00:43:20 +0800

    Add get_account_wallets utility method
    Fetch all wallets of a specific account.
    
    Signed-off-by: Jethro De Guzman <jethdeguzman@gmail.com>

--- lib/wallet_app/util.ex
+++ lib/wallet_app/util.ex
@@ -22,4 +22,32 @@ defmodule WalletApp.Util do
       _ -> raise "Invalid session token"
     end
   end
+
+  def get_account_wallets(account_id) do
+    query = """
+      select
+        w.uuid,
+        w.currency,
+        coalesce(t1.balance, 0.0000) as balance,
+        w.inserted_at
+      from
+        wallets w
+      left join (
+        select
+          t.wallet_id,
+          t.balance
+        from
+          transactions t
+        order by
+          t.wallet_id,
+          t.inserted_at desc
+        limit 1
+      ) t1 on t1.wallet_id = w.id
+      where
+        w.account_id = $1
+      order by 
+        w.inserted_at desc
+    """
+    Ecto.Adapters.SQL.query!(Repo, query, [account_id])
+  end
 end
