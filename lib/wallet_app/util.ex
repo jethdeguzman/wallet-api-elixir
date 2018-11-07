418f213575f8b8ee1c0d98d8380cff3f048e8b74
Jethro De Guzman <jethro@mergecommit.com>
2018-11-07 18:04:13 +0800

    Split get_account_wallets
    Created a separate function for getting all wallets and filtered wallets.
    
    Signed-off-by: Jethro De Guzman <jethro@mergecommit.com>

--- lib/wallet_app/util.ex
+++ lib/wallet_app/util.ex
@@ -27,30 +27,45 @@ defmodule WalletApp.Util do
     end
   end
 
-  def get_account_wallets(account_id, wallet_uuid \\ nil) do
+  def get_account_wallets(account_id) do
+    get_wallets_query(account_id)
+      |> Repo.all
+      |> get_account_wallets_response
+  end
+
+  def get_account_wallets(account_id, wallet_uuid) do
+    query = from w in get_wallets_query(account_id), where: w.uuid == ^wallet_uuid
+    query
+      |> Repo.all
+      |> get_account_wallets_response
+  end
+
+  defp get_wallets_query(account_id) do
     last_tx_query =
       from t in Transaction,
       order_by: [desc: t.inserted_at],
       limit: 1,
       select: %{wallet_id: t.wallet_id, balance: t.balance}
 
-    query =
-      from w in Wallet,
+    from w in Wallet,
       left_join: t2 in subquery(last_tx_query),
       on: t2.wallet_id == w.id,
       where: w.account_id == ^account_id,
       select: [w.uuid, w.currency, fragment("coalesce(?, 0.0) as balance", t2.balance), w.inserted_at]
+  end
 
-    query = if not is_nil(wallet_uuid), do: from(w in query, where: w.uuid == ^wallet_uuid), else: query
-
-    Enum.map Repo.all(query), fn [uuid, currency, balance, inserted_at] ->
-      %{
-        uuid: uuid,
-        currency: currency,
-        balance: balance,
-        inserted_at: inserted_at
-      }
-    end
+  defp get_account_wallets_response(wallets) do
+    wallets
+      |> Enum.map(
+        fn [uuid, currency, balance, inserted_at] ->
+          %{
+            uuid: uuid,
+            currency: currency,
+            balance: balance,
+            inserted_at: inserted_at
+          }
+        end
+      )
   end
 
   def get_wallet_transactions(account_id, wallet_uuid) do
--- lib/wallet_app/wallet.ex
+++ lib/wallet_app/wallet.ex
@@ -1,13 +1,12 @@
 defmodule WalletApp.Wallet do
-  import WalletApp.Util, only: [get_current_account: 1, get_account_wallets: 2, get_wallet_transactions: 2]
-
   alias WalletApp.Exception.{ValidationError, NotFound, GetWalletsError, GetTransactionsError}
   alias WalletApp.Repo
   alias WalletApp.Schema.Account
   alias WalletApp.Schema.Wallet
+  alias WalletApp.Util
 
   def create_wallet(session_token, currency \\ "PHP") do
-    %Account{id: account_id} = get_current_account(session_token)
+    %Account{id: account_id} = Util.get_current_account(session_token)
     %Wallet{}
       |> Wallet.changeset(%{account_id: account_id, currency: currency})
       |> Repo.insert
@@ -19,8 +18,8 @@ defmodule WalletApp.Wallet do
 
   def get_wallets(session_token) do
     with(
-      %Account{id: account_id} <- get_current_account(session_token),
-      wallets <- get_account_wallets(account_id, nil)
+      %Account{id: account_id} <- Util.get_current_account(session_token),
+      wallets <- Util.get_account_wallets(account_id)
     ) do
       wallets
     else
@@ -30,8 +29,8 @@ defmodule WalletApp.Wallet do
 
   def get_wallet(session_token, wallet_uuid) do
     with(
-      %Account{id: account_id} <- get_current_account(session_token),
-      wallets <- get_account_wallets(account_id, wallet_uuid)
+      %Account{id: account_id} <- Util.get_current_account(session_token),
+      wallets <- Util.get_account_wallets(account_id, wallet_uuid)
     ) do
       if length(wallets) > 0, do: Enum.at(wallets, 0), else: raise NotFound, wallet_uuid
     else
@@ -41,8 +40,8 @@ defmodule WalletApp.Wallet do
 
   def get_transactions(session_token, wallet_uuid) do
     with(
-      %Account{id: account_id} <- get_current_account(session_token),
-      transactions <- get_wallet_transactions(account_id, wallet_uuid)
+      %Account{id: account_id} <- Util.get_current_account(session_token),
+      transactions <- Util.get_wallet_transactions(account_id, wallet_uuid)
     ) do
       transactions
     else
