65e74dac7855b3dfddf0da6cba769a97ec663270
Jethro De Guzman <jethro@mergecommit.com>
2018-12-03 18:41:59 +0800

    Implement login and register in accounts context
    Remove old implementations
    
    Signed-off-by: Jethro De Guzman <jethro@mergecommit.com>

--- lib/wallet_app/accounts/accounts.ex
+++ lib/wallet_app/accounts/accounts.ex
@@ -1,12 +1,40 @@
 defmodule WalletApp.Accounts do
+  import Application
 
-  def register() do
+  alias WalletApp.Repo
+  alias WalletApp.Accounts.User
 
+  @one_hour 60 * 60
+  @jwt_opts %{alg: get_env(:wallet_app, :jwt_alg), key: get_env(:wallet_app, :jwt_key)}
+
+  def register(username, password) do
+    %User{}
+    |> User.changeset(%{username: username, password: password})
+    |> Repo.insert()
   end
 
-  def login() do
+  def login(username, password) do
+    with(
+      {:ok, %{uuid: uuid, password: hashed_password}} <- get_user_by(%{username: username}),
+      true <- Bcrypt.verify_pass(password, hashed_password)
+    ) do
+      generate_session_token(%{user_uuid: uuid})
+    else
+      _ -> {:error, "Invalid login credentials"}
+    end
+  end
 
+  def get_user_by(%{username: username}) do
+    case Repo.get_by(User, username: username) do
+      %User{} = user -> {:ok, user}
+      nil -> {:error, :not_found}
+    end
+  end
+
+  defp generate_session_token(%{user_uuid: user_uuid}) do
+    %{user_uuid: user_uuid, exp: DateTime.to_unix(DateTime.utc_now()) + @one_hour}
+    |> JsonWebToken.sign(@jwt_opts)
+    |> (&{:ok, &1}).()
   end
 
 end
-
--- test/wallet_app_test.exs
+++ test/wallet_app_test.exs
@@ -1,157 +1,31 @@
 defmodule WalletAppTest do
   use ExUnit.Case
 
-  alias WalletApp.Exception
-
-  @password "password"
-  @currency "PHP"
+  alias WalletApp.Accounts
+  alias WalletApp.Accounts.User
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(WalletApp.Repo)
   end
 
   test "successful account registration" do
-    assert WalletApp.register(unique_string(), @password)
+    username = "testuser"
+    password = "password"
+
+    assert {:ok, _user} = Accounts.register(username, password)
   end
 
   test "invalid username registration" do
     username = "!@#$%^"
+    password = "password"
 
-    assert_raise Exception.ValidationError, "Validation error", fn ->
-      WalletApp.register(username, @password)
-    end
+    assert {:error, %{errors: [username: _] }} = Accounts.register(username, password)
   end
 
   test "invalid password registration" do
+    username = "testuser"
     password = "pass"
 
-    assert_raise Exception.ValidationError, "Validation error", fn ->
-      WalletApp.register(unique_string(), password)
-    end
+    assert {:error, %{errors: [password: _]}} = Accounts.register(username, password)
   end
-
-  test "duplicate username registration" do
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    assert_raise Exception.ValidationError, "Validation error", fn ->
-      WalletApp.register(username, @password)
-    end
-  end
-
-  test "successful login" do
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    assert WalletApp.login(username, @password)
-  end
-
-  test "account does not exist on login" do
-    username = "does_not_exist"
-
-    assert_raise Exception.InvalidLoginCredentialsError, "Invalid login credentials", fn ->
-      WalletApp.login(username, @password)
-    end
-  end
-
-  test "password not matched on login" do
-    username = unique_string()
-    password = "invalid_password"
-
-    WalletApp.register(username, @password)
-
-    assert_raise Exception.InvalidLoginCredentialsError, "Invalid login credentials", fn ->
-      WalletApp.login(username, password)
-    end
-  end
-
-  test "succesful create wallet" do
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    session_token = WalletApp.login(username, @password)
-
-    assert WalletApp.create_wallet(session_token, @currency)
-  end
-
-  test "invalid session token on create wallet" do
-    session_token = "invalid_token"
-
-    assert_raise Exception.InvalidSessionToken, "Invalid session token: invalid_token", fn ->
-      WalletApp.create_wallet(session_token, @currency)
-    end
-  end
-
-  test "invalid currency on create wallet" do
-    currency = "BT"
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    session_token = WalletApp.login(username, @password)
-
-    assert_raise Exception.ValidationError, "Validation error", fn ->
-      WalletApp.create_wallet(session_token, currency)
-    end
-  end
-
-  test "successful get wallets" do
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    session_token = WalletApp.login(username, @password)
-    wallet_uuid = WalletApp.create_wallet(session_token, @currency)
-
-    assert [%{currency: @currency, uuid: wallet_uuid}] = WalletApp.get_wallets(session_token)
-  end
-
-  test "invalid session token on get wallets" do
-    session_token = "invalid_token"
-
-    assert_raise Exception.InvalidSessionToken, "Invalid session token: invalid_token", fn ->
-      WalletApp.get_wallets(session_token)
-    end
-  end
-
-  test "successful get wallet" do
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    session_token = WalletApp.login(username, @password)
-    wallet_uuid = WalletApp.create_wallet(session_token, @currency)
-
-    assert %{currency: @currency, uuid: wallet_uuid} =
-             WalletApp.get_wallet(session_token, wallet_uuid)
-  end
-
-  test "not found on get wallet" do
-    username = unique_string()
-    wallet_uuid = "invalid_uuid"
-
-    WalletApp.register(username, @password)
-
-    session_token = WalletApp.login(username, @password)
-
-    assert_raise Exception.NotFound, "Record not found: invalid_uuid", fn ->
-      WalletApp.get_wallet(session_token, wallet_uuid)
-    end
-  end
-
-  test "get transactions successful" do
-    username = unique_string()
-
-    WalletApp.register(username, @password)
-
-    session_token = WalletApp.login(username, @password)
-    wallet_uuid = WalletApp.create_wallet(session_token, @currency)
-
-    assert [] = WalletApp.get_transactions(session_token, wallet_uuid)
-  end
-
-  defp unique_string, do: Ecto.UUID.generate()
 end
